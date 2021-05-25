78c0e1ac88fe120ca1ddf502b7064c6b31c16e24
TheoBlm <84008030+TheoBlm@users.noreply.github.com>
2021-05-25 09:46:04 +0200

    Add files via upload

--- historique.java
+++ historique.java
@@ -1,25 +1,26 @@
 import java.util.ArrayList;
 
 public class historique{
-    public static void main(String[] args) {        //variables d'autres programmes: joueur1, victoire,position
+    public static void main(String[] args) {        
         boolean joueur1 = false ;
         boolean victoire = false ;
-        String position="test";
-        String event="";
-        remplir_historique(joueur1,victoire,position);
-        System.out.println(event);
+        int testx=0;
+        int testy=0;
+        String sortie="";
+        remplir_historique(joueur1,victoire,testx,testy);
+        System.out.println(sortie);
     }
     
-    static void remplir_historique(boolean joueur1,boolean victoire,String position){
+    static void remplir_historique(boolean joueur1,boolean victoire,int testx,int testy){
         ArrayList<String> liste_coups = new ArrayList<String>();
         int i = 0;
         while(victoire==false){
             if (joueur1){
-                liste_coups.add(position);
+                liste_coups.add("("+testx+","+testy+")");
                 joueur1=false;
                 System.out.println("Coup du joueur1: "+liste_coups.get(i));     //affichage
             }else{
-                liste_coups.add(position);
+                liste_coups.add("("+testx+","+testy+")");
                 joueur1=true;
                 System.out.println("Coup du joueur2: "+liste_coups.get(i));     //affichage
             }
@@ -33,9 +34,9 @@ public class historique{
     }
     public String toString(){
     for (int j=0;j<liste_coups.size();j++){
-        event = event+liste_coups.get(i);
+        sortie = sortie+liste_coups.get(i);
     }
-    return event;
+    return sortie;
     
 }
 }
