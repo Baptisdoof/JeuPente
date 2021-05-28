42d4f02d11c50510e481eee03fff33bd184adc8f
TheoBlm <84008030+TheoBlm@users.noreply.github.com>
2021-05-28 15:48:11 +0200

    Update historique.java

--- historique.java
+++ historique.java
@@ -7,31 +7,32 @@ public class historique{
         int testx=0;
         int testy=0;
         String sortie="";
-        remplir_historique(joueur1,victoire,testx,testy);
+        ArrayList<String> liste_coups = new ArrayList<String>();
+        remplir_historique(liste_coups,joueur1,victoire,testx,testy);
         System.out.println(sortie);
+        
     }
     
-    static void remplir_historique(boolean joueur1,boolean victoire,int testx,int testy){
-        ArrayList<String> liste_coups = new ArrayList<String>();
+    static void remplir_historique(ArrayList<String> liste_coups,boolean joueur1,boolean victoire,int testx,int testy){
         int i = 0;
-        while(victoire==false){
-            if (joueur1){
-                liste_coups.add("("+testx+","+testy+")");
-                joueur1=false;
-                System.out.println("Coup du joueur1: "+liste_coups.get(i));     //affichage
-            }else{
-                liste_coups.add("("+testx+","+testy+")");
-                joueur1=true;
-                System.out.println("Coup du joueur2: "+liste_coups.get(i));     //affichage
-            }
-            i++;   
-            //temporaire
-            if(i==5){
-                victoire=true;
-            }}
+      // while(victoire==false){
+        if (joueur1){
+            liste_coups.add("("+testx+","+testy+")");
+            joueur1=false;
+            System.out.println("Coup du joueur1: "+liste_coups.get(i));     //affichage
+        }else{
+            liste_coups.add("("+testx+","+testy+")");
+            joueur1=true;
+            System.out.println("Coup du joueur2: "+liste_coups.get(i));     //affichage
+        }
+        i++;   
+        //temporaire
+        if(i==5){
+            victoire=true;
+        }}
             
            
-    }
+    
     public String toString(){
     for (int j=0;j<liste_coups.size();j++){
         sortie = sortie+liste_coups.get(i);
@@ -40,6 +41,7 @@ public class historique{
     
 }
 }
+
 
 
 
